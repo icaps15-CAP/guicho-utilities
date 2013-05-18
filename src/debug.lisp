009bdb67a1b78a4ace9cf1d91899683301a13cae
Masataro Asai <guicho2.71828@gmail.com>
2013-05-18 17:34:57 +0900

    ensures the report is printed even if the computation is stopped

--- src/debug.lisp
+++ src/debug.lisp
@@ -11,11 +11,13 @@
   (iter (for arg in args)
 		(break "~a" arg)))
 
+@eval-always
 @export
 (defmacro with-profiling ((&key packages) &body body)
   `(progn
      ,@(iter (for package in packages)
-	     `(swank::profile-package ,package t t))
-     ,@body
-     (swank::profile-report)
-     (swank::unprofile-all)))
+	     (collect `(swank::profile-package ,package t t)))
+     (unwind-protect
+	  ,@body
+       (swank::profile-report)
+       (swank::unprofile-all))))
