45896b7a9f6579592d38ba084cb285cae5734623
Masataro Asai <guicho2.71828@gmail.com>
2013-07-04 00:19:46 +0900

    added concatenate-symbols

---
+++ src/symbol.lisp
@@ -0,0 +1,16 @@
+
+(in-package :guicho-utilities)
+
+@export
+@doc "concatenate the name of the symbol with `-' delimiter and
+intern that string in the current package."
+(defun concatenate-symbols (sym &rest syms)
+  (%concatenate-symbols syms (symbol-name sym)))
+
+(defun %concatenate-symbols (syms str)
+  (if syms
+      (%concatenate-symbols
+       (cdr syms)
+       (concatenate 'string str "-" (symbol-name (car syms))))
+      (intern str)))
+      
