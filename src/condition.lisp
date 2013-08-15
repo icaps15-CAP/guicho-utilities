3333bbae971b8cfa976128d01a90edc9bfd310c0
guicho <guicho@guicho-ThinkPad-X60.(none)>
2013-08-15 17:07:09 +0900

    restart/handler-return

--- src/condition.lisp
+++ src/condition.lisp
@@ -10,6 +10,85 @@
 	     ,@body)
 	  body)))
 
+@export
+(defmacro handler-bind* (bindings &body body)
+  `(handler-bind (,(car bindings))
+     ,(if (cdr bindings)
+	  `(handler-bind* ,(cdr bindings)
+	     ,@body)
+	  body)))
+
+
+@export
+@doc "The variation of restart-case whose behavior is the same but
+the semantics are that of RESTART-BIND.
+Just as RESTART-CASE, the condition is handled first (that is, it jumps
+out of the RESTART-BIND scope with GO) and then
+the restart function is called. Finally, RESTART-RETURN returns
+the value of restart function."
+(defmacro restart-return (bindings &body body)
+  (with-gensyms (block-name)
+    (let ((bindings2
+	   (mapcar
+	    (lambda (binding)
+	      (destructuring-bind
+		    (name function . key-value-pair)
+		  binding
+		(with-gensyms (fn-name rest)
+		  (list `(,fn-name
+			  (lambda (&rest ,rest)
+			    (return-from ,block-name
+			      (apply ,function ,rest))))
+			`(,name (lambda (&rest ,rest)
+				  (apply ,fn-name ,rest))
+				,@key-value-pair)))))
+	    bindings)))
+      `(block ,block-name
+	 (flet ,(mapcar #'first bindings2)
+	   (restart-bind
+	       ,(mapcar #'second bindings2)
+	     ,@body))))))
+
+@export
+@doc "The variation of handler-case whose behavior is the same but
+the semantics are that of HANDLER-BIND.
+Just as HANDLER-CASE, the condition is handled first (that is, it jumps
+out of the HANDLER-BIND scope with GO) and then
+the handler function is called. Finally, HANDLER-RETURN returns
+the value of handler function."
+(defmacro handler-return (bindings &body body)
+  (with-gensyms (block-name)
+    (let ((bindings2
+	   (mapcar
+	    (lambda (binding)
+	      (destructuring-bind
+		    (name function . key-value-pair)
+		  binding
+		(with-gensyms (fn-name rest)
+		  (list `(,fn-name
+			  (lambda (&rest ,rest)
+			    (return-from ,block-name
+			      (apply ,function ,rest))))
+			`(,name (lambda (&rest ,rest)
+				  (apply ,fn-name ,rest))
+				,@key-value-pair)))))
+	    bindings)))
+      `(block ,block-name
+	 (flet ,(mapcar #'first bindings2)
+	   (handler-bind
+	       ,(mapcar #'second bindings2)
+	     ,@body))))))
+
+
+;; (restart-return ((retry (lambda (c) (print :retry)))
+;; 		 (continue (lambda (c) (print :retry))))
+;;   (error "error!"))
+
+;; (restart-case
+;;     (error "error!")
+;;   (retry (c) (print :retry))
+;;   (continue (c) (print :retry)))
+
 ;; (restart-bind* ((retry (lambda (c) (print :retry)))
 ;; 		(continue (lambda (c) (print :retry))))
 ;;   (error "error!"))
