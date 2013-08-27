683598a029d9acaa65db6a3115c192f305f8731e
guicho <guicho@guicho-ThinkPad-X60.(none)>
2013-08-28 01:34:03 +0900

    indentation change

--- src/object.lisp
+++ src/object.lisp
@@ -8,16 +8,16 @@
 
 (defmethod print-object ((o recursive-print-mixin) stream)
   (print-unreadable-object (o stream :type t)
-	(format stream
-			"~<~@{~{~a: ~a~}~^~_~}~:>"
-			(mapcar (lambda (name)
-					  (list name
-							(if (slot-boundp o name)
-								(slot-value o name)
-								:unbound)))
-					(mapcar #'slot-definition-name
-							(class-slots
-							 (class-of o)))))))
+    (format stream
+	    "~<~@{~{~a: ~a~}~^~_~}~:>"
+	    (mapcar (lambda (name)
+		      (list name
+			    (if (slot-boundp o name)
+				(slot-value o name)
+				:unbound)))
+		    (mapcar #'slot-definition-name
+			    (class-slots
+			     (class-of o)))))))
 
 
 @export
@@ -39,18 +39,18 @@ instacne. ARGS specifies initialization arguments to reinitialize-instance."
   @ignore args
   (copy-hash-table s))
 (defmethod shallow-copy ((o standard-object)
-						 &rest args &key &allow-other-keys)
+			 &rest args &key &allow-other-keys)
   (let* ((class (class-of o))
-		 (new (allocate-instance class)))
-	(mapc
-	 (lambda (name)
-	   (ignore-errors
-		 (setf (slot-value new name)
-			   (slot-value o name))))
-	 (mapcar #'slot-definition-name
-			 (class-slots class)))
-	(apply #'reinitialize-instance new args)
-	new))
+	 (new (allocate-instance class)))
+    (mapc
+     (lambda (name)
+       (ignore-errors
+	 (setf (slot-value new name)
+	       (slot-value o name))))
+     (mapcar #'slot-definition-name
+	     (class-slots class)))
+    (apply #'reinitialize-instance new args)
+    new))
 
 @export
 (defclass deep-copyable () ())
@@ -63,13 +63,13 @@ instances whose classes are the subclass of `deep-copyable'."
 
 (defmethod deep-copy ((o deep-copyable) &rest args &key &allow-other-keys)
   (let* ((class (class-of o))
-		 (new (allocate-instance class)))
-	(mapc
-	 (lambda (name)
-	   (ignore-errors
-		 (setf (slot-value new name)
-			   (deep-copy (slot-value o name)))))
-	 (mapcar #'slot-definition-name
-			 (class-slots class)))
-	(apply #'reinitialize-instance new args)
-	new))
+	 (new (allocate-instance class)))
+    (mapc
+     (lambda (name)
+       (ignore-errors
+	 (setf (slot-value new name)
+	       (deep-copy (slot-value o name)))))
+     (mapcar #'slot-definition-name
+	     (class-slots class)))
+    (apply #'reinitialize-instance new args)
+    new))
