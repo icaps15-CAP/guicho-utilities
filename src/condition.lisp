d07481d51aacb4c5bed4da82443ded878903a0bf
Masataro Asai <guicho2.71828@gmail.com>
2013-10-04 14:17:00 +0900

    bugfix: the body of do-restart was not an implicit progn.

--- src/condition.lisp
+++ src/condition.lisp
@@ -96,23 +96,24 @@ the value of handler function."
 @export
 (defmacro do-restart (bindings &body body)
   (with-gensyms (start)
-    `(tagbody
+    `(block nil
+       (tagbody
         ,start
-        (block nil
-          (restart-bind
-              ,(mapcar
-                (lambda (binding)
-                  (destructuring-bind
-                        (name function . key-value-pair)
-                      binding
-                    (with-gensyms (rest)
-                      `(,name (lambda (&rest ,rest)
-                                (prog1
-                                    (apply ,function ,rest)
-                                  (go ,start)))
-                              ,@key-value-pair))))
-                bindings)
-            ,@body)))))
+          (return
+            (restart-bind
+                ,(mapcar
+                  (lambda (binding)
+                    (destructuring-bind
+                          (name function . key-value-pair)
+                        binding
+                      (with-gensyms (rest)
+                        `(,name (lambda (&rest ,rest)
+                                  (prog1
+                                      (apply ,function ,rest)
+                                    (go ,start)))
+                                ,@key-value-pair))))
+                  bindings)
+              ,@body))))))
 
 
 ;; (do-restart ((retry (lambda (c) (print :retry)))
