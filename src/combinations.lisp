83328bdffef836bcc7ddcd0b5d38b1d69469b545
Masataro Asai <guicho2.71828@gmail.com>
2014-01-08 18:52:00 +0900

    added test, fixed combinations, added `length' feature to permutations

--- src/combinations.lisp
+++ src/combinations.lisp
@@ -3,7 +3,7 @@
 (use-syntax :annot)
 
 @export
-(defun permutations (seq)
+(defun permutations (seq &key length)
   "returns a list of sequences.
    each sequence is a permutation of the original sequence.
 
@@ -23,15 +23,20 @@ Based on Steinhaus–Johnson–Trotter algorithm + Even's speedup
 
 "
   (etypecase seq
-    (cons (%permutations-of-cons seq))))
+    (cons (let ((seq-size (length seq)))
+            (if (or (null length)
+                    (= length seq-size))
+                (%permutations-of-cons seq seq-size seq-size)
+                (mappend (lambda (subseq)
+                           (%permutations-of-cons subseq length length))
+                         (%combinations-of-cons seq seq-size length)))))))
 
-(defun %permutations-of-cons (seq)
+(defun %permutations-of-cons (seq seq-size length)
   (match seq
     ((list _)
      (list seq))
     ((list* head rest-seq)
-     (let* ((len (length seq))
-            (n (1- len))) ; n -- 2, length -- 3
+     (let* ((n (1- length))) ; n -- 2, length -- 3
        (labels 
            ((insert (subperm pos)
               (let (acc (%subperm subperm))
@@ -62,8 +67,40 @@ Based on Steinhaus–Johnson–Trotter algorithm + Even's speedup
                      (forward rest pos
                               (cons (insert subperm pos) acc))))
                 (_ acc))))
-         (forward (%permutations-of-cons rest-seq) 0 nil))))))
+         (forward (%permutations-of-cons rest-seq (1- seq-size) n) 0 nil))))))
 
+;; non-tail-recursive version
+
+;; (defun %permutations-of-cons (seq length)
+;;   (match seq
+;;     ((list _)
+;;      (llist seq))
+;;     ((list* head rest-seq)
+;;      (let* ((n (1- length))) ; n -- 2, length -- 3
+;;        (labels 
+;;            ((insert (subperm pos)
+;;               (let (acc (%subperm subperm))
+;;                 (dotimes (i pos)        ; i = 0
+;;                   (push (car %subperm) acc)
+;;                   (setf %subperm (cdr %subperm)))
+;;                 (push head acc); pos = 1
+;;                 (dotimes (i (- n pos))  ; 2 - 1 = 1
+;;                   (push (car %subperm) acc)
+;;                   (setf %subperm (cdr %subperm)))
+;;                 (nreverse acc)))
+;;             (forward (subperms pos)
+;;               (match subperms
+;;                 ((cons subperm rest)
+;;                  (if (< pos n)
+;;                      (cons (insert subperm pos) (forward subperms (1+ pos)))
+;;                      (cons (insert subperm pos) (backward rest pos))))))
+;;             (backward (subperms pos)
+;;               (match subperms
+;;                 ((cons subperm rest)
+;;                  (if (< 0 pos)
+;;                      (cons (insert subperm pos) (backward subperms (1- pos)))
+;;                      (cons (insert subperm pos) (forward rest pos)))))))
+;;          (forward (%lpermutations-of-cons rest-seq n) 0))))))
 
 ;; less efficient, bad performance in both memory and speed
 ;; 
@@ -83,59 +120,27 @@ Based on Steinhaus–Johnson–Trotter algorithm + Even's speedup
 
 ;; combinations, implemented in a functional style
 
-;; http://stackoverflow.com/questions/7292370/recursive-combinations
-
-;; build_combinations (tokens, set_size)
-;;   Arrangements combos
-;;   if (set_size == 0)
-;;     combos.add ("")
-;;   else
-;;     Comment: tail_substrings of "ABC" is ("ABC", "BC", "C").
-;;     foreach tail (tail_substrings (tokens))
-;;       foreach sub_combo (build_combinations (tail, set_size-1))
-;;         combos.add (tail.first() + sub_combo)
-;;   return combos
-
-
 @export
 (defun combinations (seq &key (length (length seq)))
   "returns a list of sequences.
    each sequence is a k-combination of the original sequence."
   (etypecase seq
-    (cons (%combinations-of-cons seq length))))
+    (cons (%combinations-of-cons seq (length seq) length))))
 
 ;; non-tail-recursive
 
-;; (defun %combinations-of-cons (seq length)
-;;   (if (zerop length) ;;(= length (length seq))
-;;       (list (list))
-;;       (%per-tail length seq)))
+(defun %combinations-of-cons (seq seq-size length)
+  (multiple-value-match (values length seq)
+    ((0 _)   (list (list)))
+    ((_ nil) (list (list)))
+    ((k (cons x xs))
+     (let ((xs-size (1- seq-size)))
+       (if (<= k xs-size)
+           (append (mapcar (lambda (rest)
+                             (cons x rest))
+                           (%combinations-of-cons xs xs-size (1- k)))
+                   (%combinations-of-cons xs xs-size k))
+           (mapcar (lambda (rest)
+                     (cons x rest))
+                   (%combinations-of-cons xs xs-size (1- k))))))))
 
-;; (defun %per-tail (length tail)
-;;   (if tail
-;;       (%attach-head length tail (car tail) (%combinations-of-cons tail (1- length)))
-;;       (list)))
-
-;; (defun %attach-head (length tail head sub-combo)
-;;   (if sub-combo
-;;       (cons (cons head (car sub-combo))
-;;             (%attach-head length tail head (cdr sub-combo)))
-;;       (%per-tail length (cdr tail))))
-
-;; tail-recursive
-
-(defun %combinations-of-cons (seq length)
-  (if (zerop length) ;;(= length (length seq))
-      (list (list))
-      (%per-tail nil length seq)))
-
-(defun %per-tail (acc length tail)
-  (if tail
-      (%attach-head acc length tail (car tail) (%combinations-of-cons tail (1- length)))
-      acc))
-
-(defun %attach-head (acc length tail head sub-combo)
-  (if sub-combo
-      (%attach-head (cons (cons head (car sub-combo)) acc)
-                    length tail head (cdr sub-combo))
-      (%per-tail acc length (cdr tail))))
--- t/guicho-utilities.lisp
+++ t/guicho-utilities.lisp
@@ -68,3 +68,27 @@
       (print '(:transitive nil))
       (print cat)
       (is-true (= 2 (length cat))))))
+
+
+(test :permutations
+  (is (set-equal '((0 1 2) (1 0 2) (1 2 0) (2 1 0) (2 0 1) (0 2 1))
+                 (permutations '(0 1 2))
+                 :test #'equal))
+  (is (set-equal '((0 1) (0 2)
+                   (1 0) (1 2)
+                   (2 0) (2 1))
+                 (permutations '(0 1 2) :length 2)
+                 :test #'equal)))
+
+(test :combinations
+  (is (equal '(())
+             (combinations '(0 1 2) :length 0)))
+  (is (equal '((0) (1) (2))
+             (combinations '(0 1 2) :length 1)))
+  (is (equal '((0 1) (0 2) (1 2))
+             (combinations '(0 1 2) :length 2)))
+  (is (equal '((0 1 2))
+             (combinations '(0 1 2) :length 3)))
+  (is (equal '((0 1 2))
+             (combinations '(0 1 2)))))
+
