c88da5515175d59ce1658ba8df940b11d6ee1c86
Masataro Asai <guicho2.71828@gmail.com>
2015-03-30 15:43:19 +0900

    testing: more-labels broke the tests on ask-for

--- t/more-labels.lisp
+++ t/more-labels.lisp
@@ -6,38 +6,38 @@
 
 (test local-function
   (finishes
-    (define-local-function fn1 (x y)
+    (define-local-function lfn1 (x y)
       (+ x y)))
   (finishes
-    (define-local-function fn2 (x y)
-      (return-from fn2
+    (define-local-function lfn2 (x y)
+      (return-from lfn2
         (* x y))))
   (finishes
-    (define-local-function fn3 (x y)
-      (+ (fn2 x y) (fn1 x y)))))
+    (define-local-function lfn3 (x y)
+      (+ (lfn2 x y) (lfn1 x y)))))
 
 (test (more-labels :depends-on local-function)
   (is (= 7
-         (more-labels () (fn1)
-           (fn1 2 5)))))
+         (more-labels () (lfn1)
+           (lfn1 2 5)))))
 
 (test (return-from :depends-on local-function)
   ;; ensure `return-from' works correctly
   (is (= 10
-         (more-labels () (fn2)
-           (fn2 2 5)))))
+         (more-labels () (lfn2)
+           (lfn2 2 5)))))
 
 (test (interaction :depends-on local-function)
-  ;; fn3 uses fn1 and fn2
+  ;; lfn3 uses lfn1 and lfn2
   ;; test both inlined and not-inlined version
   (is (= 17
-         (more-labels () (fn1 fn2 fn3)
-           (fn3 2 5)))))
+         (more-labels () (lfn1 lfn2 lfn3)
+           (lfn3 2 5)))))
 
 (test (overwrite :depends-on local-function)
-  ;; ensure overwriting `fn2' does not affect the local definition
-  (defun fn2 (x y)
+  ;; ensure overwriting `lfn2' does not affect the local definition
+  (defun lfn2 (x y)
     (/ x y))
   (is (= 10
-         (more-labels () (fn2)
-           (fn2 2 5)))))
+         (more-labels () (lfn2)
+           (lfn2 2 5)))))
