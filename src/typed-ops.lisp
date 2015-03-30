d57bfab83ebb446e12b310efe2d8e42cab85a215
Masataro Asai <guicho2.71828@gmail.com>
2015-03-30 16:32:02 +0900

    remove docs in typed-ops

--- src/typed-ops.lisp
+++ src/typed-ops.lisp
@@ -3,11 +3,11 @@
 (use-syntax :annot)
 
 @export
-@doc "type: evaluated. @example (define-typed-op d+ + 'double-float)"
 (defmacro define-typed-op (name op type
                            &key
                            (return-value-on t)
                            (input-value-on t))
+  "type: evaluated. @example (define-typed-op d+ + 'double-float)"
   `(defmacro ,name (&rest args)
      (let ((the-args
             ,(if input-value-on
@@ -22,17 +22,16 @@
 
 ;; @eval-always
 ;; @export
-;; @doc "defaulted to double-float."
 ;; (defvar *desired-type* 'double-float)
 
 @export
-@doc "the type of computation in lmates, defaulted to `double-float.'"
 (deftype *desired-type* ()
+  "the type of computation in lmates, defaulted to `double-float.'"
   'double-float)
 
 @export
-@doc "coerce ARG into *DESIRED-TYPE* "
 (defun desired (arg)
+  "coerce ARG into *DESIRED-TYPE* "
   (coerce arg '*desired-type*))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -40,135 +39,103 @@
 
 
 @export
-@doc "typed +. see utility/typed-op"
 (define-typed-op d+ + *desired-type*)
 @export
-@doc "typed *. see utility/typed-op"
 (define-typed-op d* * *desired-type*)
 @export
-@doc "typed -. see utility/typed-op"
 (define-typed-op d- - *desired-type*)
 @export
-@doc "typed /. see utility/typed-op"
 (define-typed-op d/ / *desired-type*)
 @export
-@doc "typed sqrt. see utility/typed-op"
 (define-typed-op dsqrt sqrt *desired-type*)
 
 
 @export
-@doc "typed aref. see utility/typed-op"
 (define-typed-op daref aref *desired-type* :input-value-on nil)
 
 @export
-@doc "typed min. see utility/typed-op"
 (define-typed-op dmin min *desired-type*)
 
 
 @export
-@doc "typed max. see utility/typed-op"
 (define-typed-op dmax max *desired-type*)
 
 
 @export
-@doc "typed >. see utility/typed-op"
 (define-typed-op d> > *desired-type* :return-value-on nil)
 
 @export
-@doc "typed <. see utility/typed-op"
 (define-typed-op d< < *desired-type* :return-value-on nil)
 
 @export
-@doc "typed >=. see utility/typed-op"
 (define-typed-op d>= >= *desired-type* :return-value-on nil)
 
 @export
-@doc "typed <. see utility/typed-op"
 (define-typed-op d<= <= *desired-type* :return-value-on nil)
 
 
 @export
-@doc "typed =. see utility/typed-op"
 (define-typed-op d= = *desired-type* :return-value-on nil)
 
 @export
-@doc "typed random. see utility/typed-op"
 (define-typed-op drandom random *desired-type*)
 
 @export
-@doc "typed slot-value. see utility/typed-op"
 (define-typed-op dslot-value slot-value *desired-type* :input-value-on nil)
 
 @export
-@doc "typed setf. see utility/typed-op"
 (define-typed-op dsetf setf *desired-type* :input-value-on nil)
 
 
 @export
-@doc "typed setf. see utility/typed-op"
 (define-typed-op dabs abs *desired-type* :return-value-on nil)
 
 @export
-@doc "typed setf. see utility/typed-op"
 (define-typed-op dplusp plusp *desired-type* :return-value-on nil)
 
 @export
-@doc "typed setf. see utility/typed-op"
 (define-typed-op dminusp minusp *desired-type* :return-value-on nil)
 
 @export
-@doc "typed setf. see utility/typed-op"
 (define-typed-op dzerop zerop *desired-type* :return-value-on nil)
 
 
 @export
-@doc "typed setf. see utility/typed-op"
 (define-typed-op dcos cos *desired-type*)
 
 @export
-@doc "typed setf. see utility/typed-op"
 (define-typed-op dsin sin *desired-type*)
 
 @export
-@doc "typed setf. see utility/typed-op"
 (define-typed-op dtan tan *desired-type*)
 
 @export
-@doc "typed setf. see utility/typed-op"
 (define-typed-op dcosh cosh *desired-type*)
 
 @export
-@doc "typed setf. see utility/typed-op"
 (define-typed-op dsinh sinh *desired-type*)
 
 @export
-@doc "typed setf. see utility/typed-op"
 (define-typed-op dtanh tanh *desired-type*)
 
 
 @export
-@doc "typed setf. see utility/typed-op"
 (define-typed-op dacos acos *desired-type*)
 
 @export
-@doc "typed setf. see utility/typed-op"
 (define-typed-op dasin asin *desired-type*)
 
 @export
-@doc "typed setf. see utility/typed-op"
 (define-typed-op datan atan *desired-type*)
 
 
 @export
-@doc "typed setf. see utility/typed-op"
 (define-typed-op dexp exp *desired-type*)
 
 @export
-@doc "typed setf. see utility/typed-op"
 (define-typed-op dexpt expt *desired-type*)
 
 @export
-@doc "typed setf. see utility/typed-op"
 (define-typed-op dlog log *desired-type*)
 
 
@@ -176,21 +143,18 @@
 ;;;; constants
 
 @export
-@doc "eps of double float"
-(defconstant +eps+ 1.0d-10)
+(defconstant +eps+ 1.0d-10 "eps of double float")
 @export
-@doc "delta for approximation"
-(defparameter +delta+ 1.0d-2)
+(defparameter +delta+ 1.0d-2 "delta for approximation")
 @export
-@doc "around 6.28"
-(defconstant +2pi+ (d* 2.0d0 pi))
+(defconstant +2pi+ (d* 2.0d0 pi) "around 6.28")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; misc
 
 @export
-@doc "approximated ="
 (defun =~ (&rest args)
+  "approximated ="
   (let ((x1 (first args))
         (x2 (second args)))
     (and (< (abs (- x2 x1)) +delta+)
@@ -198,8 +162,8 @@
              (apply #'=~ (cdr args))))))
 
 @export
-@doc "approximated ="
 (defun d=~ (&rest args)
+  "approximated ="
   (let ((x1 (first args))
         (x2 (second args)))
     @type *desired-type* x1
